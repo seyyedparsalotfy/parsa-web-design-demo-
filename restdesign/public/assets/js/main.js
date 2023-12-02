1a9a2b8224a0c243b695608fc7386370a72c73e8
project <139109816+seyyedparsalotfy@users.noreply.github.com>
2023-12-02 15:23:07 +0000

    Update main.js and index.html

--- restdesign/public/assets/js/main.js
+++ restdesign/public/assets/js/main.js
@@ -1,80 +1,101 @@
-const showMenu = (e, t) => {
-  const o = document.getElementById(e),
-    n = document.getElementById(t);
-  o &&
-    n &&
-    o.addEventListener("click", () => {
-      n.classList.toggle("show-menu");
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
     });
+  }
 };
 showMenu("nav-toggle", "nav-menu");
+
 const navLink = document.querySelectorAll(".nav__link");
+
 function linkAction() {
-  const e = document.getElementById("nav-menu");
-  e.classList.remove("show-menu");
+  const navMenu = document.getElementById("nav-menu");
+
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach((e) => e.addEventListener("click", linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
 const sections = document.querySelectorAll("section[id]");
+
 function scrollActive() {
-  var n = window.pageYOffset;
-  sections.forEach((e) => {
-    var t = e.offsetHeight,
-      o = e.offsetTop - 50;
-    (sectionId = e.getAttribute("id")),
-      o < n && n <= o + t
-        ? document
-            .querySelector(".nav__menu a[href*=" + sectionId + "]")
-            .classList.add("active-link")
-        : document
-            .querySelector(".nav__menu a[href*=" + sectionId + "]")
-            .classList.remove("active-link");
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
   });
 }
+window.addEventListener("scroll", scrollActive);
+
 function scrollHeader() {
-  const e = document.getElementById("header");
-  200 <= this.scrollY
-    ? e.classList.add("scroll-header")
-    : e.classList.remove("scroll-header");
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
+window.addEventListener("scroll", scrollHeader);
+
 function scrollTop() {
-  const e = document.getElementById("scroll-top");
-  560 <= this.scrollY
-    ? e.classList.add("show-scroll")
-    : e.classList.remove("show-scroll");
+  const scrollTop = document.getElementById("scroll-top");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("show-scroll");
 }
-window.addEventListener("scroll", scrollActive),
-  window.addEventListener("scroll", scrollHeader),
-  window.addEventListener("scroll", scrollTop);
-const themeButton = document.getElementById("theme-button"),
-  darkTheme = "dark-theme",
-  iconTheme = "bx-sun",
-  selectedTheme = localStorage.getItem("selected-theme"),
-  selectedIcon = localStorage.getItem("selected-icon"),
-  getCurrentTheme = () =>
-    document.body.classList.contains(darkTheme) ? "dark" : "light",
-  getCurrentIcon = () =>
-    themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
-selectedTheme &&
-  (document.body.classList["dark" === selectedTheme ? "add" : "remove"](
+window.addEventListener("scroll", scrollTop);
+
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-sun";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "bx-moon" : "bx-sun";
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
     darkTheme,
-  ),
-  themeButton.classList["bx-moon" === selectedIcon ? "add" : "remove"](
+  );
+  themeButton.classList[selectedIcon === "bx-moon" ? "add" : "remove"](
     iconTheme,
-  )),
-  themeButton.addEventListener("click", () => {
-    document.body.classList.toggle(darkTheme),
-      themeButton.classList.toggle(iconTheme),
-      localStorage.setItem("selected-theme", getCurrentTheme()),
-      localStorage.setItem("selected-icon", getCurrentIcon());
-  });
+  );
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
+
 const sr = ScrollReveal({
   origin: "top",
   distance: "30px",
-  duration: 2e3,
-  reset: !0,
+  duration: 2000,
+  reset: true,
 });
+
 sr.reveal(
-  ` .home__data, .home__img,
+  `.home__data, .home__img,
             .about__data, .about__img,
             .services__content, .menu__content,
             .app__data, .app__img,
